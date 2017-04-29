12f05bcc19167db4e4dbef57640a1d04cd2def17
yousefnabil48 <youssefnabil48@outlook.com>
2017-04-29 11:06:52 +0200

    Tv client registration fixed

--- SmartHomeBoard/src/dbAccess/DeviceQueries.java
+++ SmartHomeBoard/src/dbAccess/DeviceQueries.java
@@ -13,9 +13,8 @@ public class DeviceQueries {
 		this.device = device;
 	}
 	public void registerDevice() throws SQLException{
-		String statement="insert into Sensor values ('"+ device.getName().toString() +"',7,'"+
+		String statement="insert into Sensor values ('"+ device.getName().toString() +"',"+ String.valueOf(this.getLastID()+1)+",'"+
 		String.valueOf(device.getSerialNumber())+"','"+String.valueOf(device.getPortNumber())+"','20393933X','Type',true,true,true,233)";
-		//String statement = "insert into Sensor values ('tv',3,'24442','3424','AKOEO29301','fun',true,true,true,233)";
 		System.out.println("sql statment: "+statement);
 		databaseConnection.executeUpdate(statement);
 		System.out.println("query executed");
--- TvClient/src/Registeration.java
+++ TvClient/src/Registeration.java
@@ -1,40 +1,43 @@
+package src;
 
+import java.net.URI;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-
-import com.owlike.genson.stream.JsonType;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.GenericType;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.core.spi.factory.MessageBodyFactory;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.jersey.client.ClientConfig;
+import org.json.simple.parser.*;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import javax.persistence.*;
+import javax.ws.rs.client.Entity;
 
 public class Registeration {
-	
+
 	@SuppressWarnings("unchecked")
-	public static void register(){
-<<<<<<< HEAD
-	JSONObject body = new JSONObject();
-	body.put("deviceName", "TV");
-	body.put("serialNumber", "2374648");
-	body.put("portNumber", "2222");
-	System.out.println(body.toString());
-	Client client = Client.create();
-	WebResource webResource = client.resource("http://localhost:8080/SmartHomeBoard/service/device/register");
-	String response = webResource.type(MediaType.APPLICATION_JSON)
-=======
+	public static void register() {
 		JSONObject body = new JSONObject();
 		body.put("deviceName", "TV");
 		body.put("serialNumber", "2374648");
 		body.put("portNumber", "2222");
-		System.out.println(body.toJSONString());
-		Client client = Client.create();
-		WebResource webResource = client.resource("http://localhost:8080/SmartHomeBoard/service/device/register");
-		String response = webResource.type(MediaType.APPLICATION_JSON)
->>>>>>> b7db6bc54abaef7f4ff3f0dd3f71ee0ceb8aeb11
-					  .accept(MediaType.APPLICATION_JSON)
-					  .post(new GenericType<String>(){},body.toJSONString());
-		System.out.println(response);
+		System.out.println(body.toString());
+		ClientConfig config1 = new ClientConfig();
+
+		Client client = ClientBuilder.newClient(config1);
+
+		WebTarget target = client.target(UriBuilder.fromUri(
+				"http://localhost:8080/SmartHomeBoard/service/device/register")
+				.build());
+		Object obj = target
+				.request()
+				.accept(MediaType.APPLICATION_JSON)
+				.post(Entity.entity(body.toJSONString(),
+						MediaType.APPLICATION_JSON), String.class);
+		JSONObject response = (JSONObject) obj;
+		System.out.println(response.toJSONString());
+
 	}
 }
